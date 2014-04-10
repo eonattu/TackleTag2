4478f85374e6192d6cb457bb35c5799fd084ea1d
adriangerbaud <agerbau2@nd.edu>
2014-04-10 11:07:34 -0400

    Edwin

--- background.h
+++ background.h
@@ -1,5 +1,5 @@
 //Authors: Adrian A. Gerbaud H., Edwin Onattu, Alejandra Aranguren
-// This is the header file for the Background class, which handles our field background.
+/// This is the header file for the Background class, which handles our field background.
 
 #include "SDL/SDL.h"
 using namespace std;
